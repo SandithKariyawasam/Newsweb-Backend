1846ddc022d17293a6fcb522bdee8cc7d88aa8b0
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-08-08 17:27:34 +0530

    Create database connection

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8070;
+
+// CORS configuration
+// app.use(cors({
+//     origin: "https://next-news-portal-seven.vercel.app/",
+//     credentials: true 
+// }));
+
+app.use(cors());
+
+
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 10000, // Timeout after 10 seconds instead of 30 seconds
+})
+    .then(() => console.log("Mongodb Connection Success!"))
+    .catch((err) => console.error("Mongodb Connection Failed: ", err));
+
+const newsRoutes = require("./routes/newsRoutes.js");
+app.use("/news", newsRoutes);
+
+const userRouter = require("./routes/users.js");
+app.use("/users", userRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port number: ${PORT}`);
+});
