1493038fdf94fcaced24bef32f4f5d5efa9325e8
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-12-07 16:06:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config(); 
+
 const app = express();
-require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.status(200).json({ message: "Server is up and running!" });
-});
+const PORT = process.env.PORT || 8070;
 
 // CORS configuration
 app.use(cors({
@@ -20,9 +18,6 @@ app.use(cors({
     credentials: true
 }));
 
-// app.use(cors());
-
-
 app.use(bodyParser.json());
 
 const URL = process.env.MONGODB_URL;
@@ -30,17 +25,26 @@ const URL = process.env.MONGODB_URL;
 mongoose.connect(URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 10000, // Timeout after 10 seconds instead of 30 seconds
+    serverSelectionTimeoutMS: 10000, // Timeout after 10 seconds
 })
     .then(() => console.log("Mongodb Connection Success!"))
     .catch((err) => console.error("Mongodb Connection Failed: ", err));
 
+app.get("/", (req, res) => {
+    res.status(200).json({ 
+        message: "Server is up and running!", 
+        status: "Active" 
+    });
+});
+
+// Routes
 const newsRoutes = require("./routes/newsRoutes.js");
 app.use("/news", newsRoutes);
 
 const userRouter = require("./routes/users.js");
 app.use("/users", userRouter);
 
+// Start Server
 app.listen(PORT, () => {
     console.log(`Server is up and running on port number: ${PORT}`);
 });
